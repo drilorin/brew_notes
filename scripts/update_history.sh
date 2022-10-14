b6584cdf5e260f559b2dbe60ae9da48ad4d8de38
Dennis Rilorin <dennis@redpill-linpro.com>
2022-10-14 13:34:18 +0200

    Minor change

--- scripts/update_history.sh
+++ scripts/update_history.sh
@@ -8,5 +8,5 @@ for i in $(ls -1 ../history/brew*) ;
 do DATE=`head -4 $i | grep Date | awk -F: '{print $2}' | awk '{print $1}'` ;
 NAME=`echo $i | awk -F/ '{print $3}' | awk -F. '{print $1}'`
 FILE=`echo $i | awk -F/ '{print $3}'`
-echo "$DATE [$NAME]($FILE)" >> ../history/README.md ; 
+echo " - $DATE [$NAME]($FILE)" >> ../history/README.md ; 
 done
